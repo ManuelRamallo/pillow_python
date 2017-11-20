8f439e3d54188158665ddbaa375182cdc96d4ac5
ManuelRamallo <mramallodiaz96@gmail.com>
2017-11-20 20:03:38 +0100

    creado repositorio de Pillow y agregado el proyecto de Pillow-Python

---
+++ pillow_env/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from PIL import Image, ImageChops, ImageEnhance, ImageOps
+
+def main():
+
+    #Esto abre una imagen que esté dentro de nuestro proyecto
+    imagen = Image.open('imagen.jpg')
+    #Esta linea hace que muestre la foto cuando se ejecute el programa
+    imagen.show()
+
+    #Ejemplo para invertir los colores de la imagen
+    new_imagen = ImageChops.invert(imagen)
+    new_imagen.save("imagen_2.jpg")
+
+    #Ejemplo para cambiar la escala de grises de la imagen
+    new_imagen = ImageOps.grayscale(imagen)
+    new_imagen.save("imagen_3.jpg")
+
+    #Ejemplo para subir el brillo de la imagen
+    new_imagen = ImageEnhance.Brightness(imagen).enhance(2)
+    new_imagen.save("imagen_4.jpg")
+
+    #Ejemplo para aumentar el contraste de la imagen
+    new_imagen = ImageEnhance.Contrast(imagen).enhance(4)
+    new_imagen.save("imagen_5.jpg")
+
+    #Ejemplo para voltear una imagen como si fuera un espejo
+    new_imagen = ImageOps.mirror(imagen)
+    new_imagen.save("imagen_6.jpg")
+
+    #Cambia el tamaño de la imagen
+    new_imagen = imagen.resize((320, 240))
+    new_imagen.save("imagen_7.jpg")
+
+    #Ejemplo para disminuir la tinidez de la imagen
+    new_imagen = ImageEnhance.Sharpness(imagen).enhance(-4)
+    new_imagen.save("imagen_8.jpg")
+
+
+
+if __name__ == "__main__":
+    main()
